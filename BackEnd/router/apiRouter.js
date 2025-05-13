b5afb215ae8887aa4d419eb7ed202d5edefd82b2
kinJH <kmc010107@gmail.com>
2025-05-13 22:13:06 +0900

    중복체크

--- BackEnd/router/apiRouter.js
+++ BackEnd/router/apiRouter.js
@@ -6,12 +6,15 @@ router.get('/idcheck/:id',async (req, res)=>{
     const id = req.params.id;
 
     const userInfo = await authRepo.getUserById(id);
-
-    if(userInfo.id){
-        return res.send('중복 ID')
-    }
-    else{
-        res.send(`${id} 사용가능`)
+    try {
+        if(userInfo.id){
+            return res.send('중복 ID')
+        }
+        else{
+            res.send(`${id} 사용가능`)
+        }
+    } catch (error) {
+        res.send('사용불가능 Id')
     }
     
 })
