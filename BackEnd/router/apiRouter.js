8ed2fc9f32a28a2f365056eb0820090dc1b86353
kinJH <kmc010107@gmail.com>
2025-05-13 22:01:06 +0900

    중복체크

---
+++ BackEnd/router/apiRouter.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const authRepo = require('../repository/authRepo');
+const router = express.Router();
+
+router.get('/idcheck/:id',async (req, res)=>{
+    const id = req.params.id;
+
+    const userInfo = await authRepo.getUserById(id);
+    if(userInfo.id){
+        return res.send('중복 ID')
+    }
+    else{
+        res.send(`${id} 사용가능`)
+    }
+    
+})
+
+module.exports = router;
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -8,6 +8,7 @@ const port = 4000;
 
 const db = require('./db.js');
 const authRouter = require('./router/authRouter.js');
+const apiRouter = require('./router/apiRouter.js')
 
 // ✅ CORS 설정 (프론트엔드와 세션 공유 위해 필요)
 app.use(cors({
@@ -36,6 +37,7 @@ db.connect((err) => {
 
 // ✅ 라우터 등록
 app.use('/auth', authRouter);
+app.use('/api',apiRouter)
 
 // 테스트용 API
 app.get('/', (req, res) => {
