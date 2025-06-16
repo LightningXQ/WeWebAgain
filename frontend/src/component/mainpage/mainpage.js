947c95ef0b697f8b2ab33d0651a01de820a884a4
LightningXQ <jihoj20030501@gmail.com>
2025-06-16 20:38:05 +0900

    poi start search

--- frontend/src/component/mainpage/mainpage.js
+++ frontend/src/component/mainpage/mainpage.js
@@ -7,6 +7,7 @@ import axios from "axios";
 
 // Material-UI
 import {
+	Autocomplete,
 	Box,
 	Card,
 	Stack,
@@ -36,212 +37,246 @@ const weather = "/images/weather_example.png";
 const food = "/images/food.png";
 
 const theme = createTheme({
-   components: {
-    MuiOutlinedInput: {
-      styleOverrides: {
-        root: {
-          fontSize: 20,
-               borderRadius: 20,
-               ' .MuiOutlinedInput-notchedOutline': {
-            borderColor: '#AAAAAA', 
-                  borderWidth: 2,
-          },
-               ':hover .MuiOutlinedInput-notchedOutline': {
-            borderColor: '#AAAAAA', 
-                  borderWidth: 2,
-          },
-               '&.Mui-focused': {
-                  backgroundColor: '#EBF4F6',
-               },
-               '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                  borderColor: '#60B5FF', 
-               },
-        },
-      },
-    },
-  },
+	components: {
+		MuiOutlinedInput: {
+			styleOverrides: {
+				root: {
+					fontSize: 20,
+					borderRadius: 20,
+					' .MuiOutlinedInput-notchedOutline': {
+						borderColor: '#AAAAAA',
+						borderWidth: 2,
+					},
+					':hover .MuiOutlinedInput-notchedOutline': {
+						borderColor: '#AAAAAA',
+						borderWidth: 2,
+					},
+					'&.Mui-focused': {
+						backgroundColor: '#EBF4F6',
+					},
+					'&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+						borderColor: '#60B5FF',
+					},
+				},
+			},
+		},
+	},
 });
 
 const Mainpage = () => {
-   const [mode, setMode] = useState(false);
-   const [startLocation, setStartLocation] = useState("");
-   const [endLocation, setEndLocation] = useState("");
-   const [arrivalTime, setArrivalTime] = useState(null);
-   const [arrivalDate, setArrivalDate] = useState(null);
+	const [mode, setMode] = useState(false);
+	const [startLocation, setStartLocation] = useState("");
+	const [endLocation, setEndLocation] = useState("");
+	const [arrivalTime, setArrivalTime] = useState(null);
+	const [arrivalDate, setArrivalDate] = useState(null);
 
-   const handleSearch = () => {
-      console.log(startLocation, endLocation, arrivalDate, arrivalDate);
-      return;
-   }
+	const [searchResult, setSearchResult] = useState([]);
 
-   useEffect(() => {
-      const getUserInfo = async () => {
-         try {
-            const response = await axios.get('http://localhost:4000/auth/check', 
-            {
-               withCredentials: true,
-               headers: {
-                  'Content-Type': 'application/json'
-               }
-            });
-            console.log(response.data);
-            setMode(response.data.loggedIn);
-            return;
-         } catch (error) {
-            console.error(error);
-            return;
-         }
-      };
+	const handleSearch = () => {
+		console.log(startLocation, endLocation, arrivalDate, arrivalDate);
+		return;
+	}
 
-      getUserInfo();
-   }, []);
+	useEffect(() => {
+		const getUserInfo = async () => {
+			try {
+				const response = await axios.get('http://localhost:4000/auth/check',
+					{
+						withCredentials: true,
+						headers: {
+							'Content-Type': 'application/json'
+						}
+					});
+				console.log(response.data);
+				setMode(response.data.loggedIn);
+				return;
+			} catch (error) {
+				console.error(error);
+				return;
+			}
+		};
 
-   useEffect(() => {
-      // const apiTest = async () => {
-      //    try {
-      //       const response = await axios.get('https://apis.data.go.kr/1613000/RouteCongestionLevel/getRouteCongestionLevel?serviceKey=EvKLpvREUknBQwnfnB%2BFTdLwm6XJvZ3qLZuP8TuJO9DNrdO1iooes0295IrgsNO9Rcia4ahjp2yx8fyhhvuUbg%3D%3D&pageNo=1&numOfRows=10&opr_ymd=20250409&ctpv_cd=26&sgg_cd=26170&rte_id=26003007&sttn_id=2621886&dataType=JSON',
-      //       {
-      //          headers: {
-      //             'accept': '*/*'
-      //          }
-      //       });
-      //       console.log(response);
-      //       return;
-      //    } catch (error) {
-      //       console.log(error);
-      //       return;
-      //    }
-      // }
-      // apiTest();
+		getUserInfo();
+	}, []);
 
-      const apiTest1 = async () => {
-         try {
-            let responseList = []
-            for (let number = 1; number < 10; number++) {
-               const response = await axios.get(`https://apis.data.go.kr/1613000/BusRoutespecificStopInformation/getBusRoutespecificStopInformation?serviceKey=EvKLpvREUknBQwnfnB%2BFTdLwm6XJvZ3qLZuP8TuJO9DNrdO1iooes0295IrgsNO9Rcia4ahjp2yx8fyhhvuUbg%3D%3D&pageNo=${number}&numOfRows=10&opr_ymd=20250405&ctpv_cd=26&sgg_cd=26380&dataType=JSON`,
-                  {
-                     headers: {
-                        'accept': '*/*'
-                     }
-                  }
-               );
-               for (let i = 0; i < 10; i++) {
-                  responseList.push(response.data.Response.body.items.item[i])
-               }
-            }
-            console.log(responseList);
-            // const response = await axios.get(`https://apis.data.go.kr/1613000/BusRoutespecificStopInformation/getBusRoutespecificStopInformation?serviceKey=EvKLpvREUknBQwnfnB%2BFTdLwm6XJvZ3qLZuP8TuJO9DNrdO1iooes0295IrgsNO9Rcia4ahjp2yx8fyhhvuUbg%3D%3D&pageNo=1&numOfRows=10&opr_ymd=20250405&ctpv_cd=26&sgg_cd=26380&dataType=JSON`,
-            //    {
-            //       headers: {
-            //          'accept': '*/*'
-            //       }
-            //    }
-            // );
-            // console.log(response.data.Response.body.items.item[0]);
-            return;
-         } catch (error) {
-            console.log(error);
-            return;
-         }
-      }
-      apiTest1();
-   } ,[])
-   
-   return (
-      <ThemeProvider theme={theme}>
-         {/* 전체 화면 배경 */}
-         <GradientBackground cover={cover}>
-            {/* 상단 네비게이션 바 */}
-            <CustomAppBar isLogin={mode} />
-            <Box sx={{
-               flex: 1,
+	// useEffect(() => {
+	// 	const apiTest1 = async () => {
+	// 		try {
+	// 			let responseList = []
+	// 			for (let number = 1; number < 10; number++) {
+	// 				const response = await axios.get(`https://apis.data.go.kr/1613000/BusRoutespecificStopInformation/getBusRoutespecificStopInformation?serviceKey=EvKLpvREUknBQwnfnB%2BFTdLwm6XJvZ3qLZuP8TuJO9DNrdO1iooes0295IrgsNO9Rcia4ahjp2yx8fyhhvuUbg%3D%3D&pageNo=${number}&numOfRows=10&opr_ymd=20250405&ctpv_cd=26&sgg_cd=26380&dataType=JSON`,
+	// 					{
+	// 						headers: {
+	// 							'accept': '*/*'
+	// 						}
+	// 					}
+	// 				);
+	// 				for (let i = 0; i < 10; i++) {
+	// 					responseList.push(response.data.Response.body.items.item[i])
+	// 				}
+	// 			}
+	// 			console.log(responseList);
+	// 			return;
+	// 		} catch (error) {
+	// 			console.log(error);
+	// 			return;
+	// 		}
+	// 	}
 
-               display: "flex",
-               flexDirection: "row",
+	// 	apiTest1();
 
-               width: "100%",
-               marginTop: 10,
-            }}>
-               <Box sx={{
-                  flex: 2.8,
-                  backgroundColor: "#F8F8F8",
-                  padding: 4,
-                  borderRight: "1.5px solid #AAAAAA"
-               }}>
-                  <Box my={2}>
-                     <Typography fontSize={24} mb={2} fontWeight="bold">
-                        위치 설정
-                     </Typography>
-                     <TextField fullWidth placeholder="출발지를 입력하세요." value={startLocation} onChange={(e) => setStartLocation(e.target.value)} sx={{
-                        marginBottom: 2,
-                     }} />
-                     <TextField fullWidth placeholder="도착지를 입력하세요." value={endLocation} onChange={(e) => setEndLocation(e.target.value)} sx={{
-                        marginBottom: 6,
-                     }} />
-                     <Typography fontSize={24} mb={2} fontWeight="bold" sx={{
-                     }}>
-                        도착 예정 시간 설정
-                     </Typography>
-                     <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <Stack direction="row" spacing={2} sx={{
-                           justifyContent: "space-evenly",
-                        }}>
-                           <DesktopDatePicker label="날짜 선택" value={arrivalDate} onChange={(newValue) => setArrivalDate(newValue)} sx={{ width: 200, }}/>
-                           <DesktopTimePicker label="시간 선택" value={arrivalTime} onChange={(newValue) => setArrivalTime(newValue)} sx={{ width: 200, }}/>
-                        </Stack>
-                     </LocalizationProvider>
-                     <GradientButton variant="contained" onClick={handleSearch} sx={{
-                        marginTop: 4,
-                        borderRadius: 8,
-                        paddingX: 4,
-                        paddingY: 1.5,
-                        width: "100%"
-                     }}
-                     >
-                        설정 완료
-                     </GradientButton>
-                  </Box>
-               </Box>
-               <Stack direction="row" spacing={4} sx={{
-                  flex: 7,
-                  '& > *': {  // Stack의 모든 직계 자식 요소에 적용
-                     flex: 1,
-                     minWidth: 0  // flex item이 부모 컨테이너를 넘어가지 않도록 함
-                  },
-                  margin: 4,
-               }}>
-                  <Card sx={{
-                     borderRadius: 8,
-                     border: "1.5px solid #3644C9"
-                  }}>
-                     <NaverMap />
-                  </Card>
-                  <Stack direction="column" spacing={4} sx={{
-                     '& > *': {  // Stack의 모든 직계 자식 요소에 적용
-                        flex: 1,
-                        minWidth: 0  // flex item이 부모 컨테이너를 넘어가지 않도록 함
-                     },
-                     margin: 4,
-                  }}>
-                     <Card sx={{
-                        borderRadius: 8,
-                        border: "1.5px solid #AAAAAA",
-                     }}>
-                        <Weather />
-                     </Card>
-                     <Card sx={{
-                        borderRadius: 8,
-                        display: "flex",
-                        border: "1.5px solid gray",
-                     }}>
-                        {/* <img src={food} style={{ width: "543px", height: "382px" }}></img> */}
-                     </Card>
-                  </Stack>
-               </Stack>
-            </Box>
-         </GradientBackground>
-      </ThemeProvider>
-   )
+	// }, [])
+
+	useEffect(() => {
+		const apiTest2 = async () => {
+			try {
+				const response = await axios.get(
+					"https://apis.openapi.sk.com/tmap/pois",
+					{
+						params: {
+							version: "1",
+							format: "json",
+							searchKeyword: startLocation,
+							resCoordType: "EPSG3857",
+							reqCoordType: "WGS84GEO",
+							count: 10,
+						},
+						headers: {
+							appKey: "WhGcxVojKO7g0CL8lD1NYgw2TiEf2r25qFNDUpOd",
+						},
+					}
+				);
+
+
+				/** POI 이름만 추출해서 한 번에 state 교체 */
+				const names = response.data.searchPoiInfo.pois.poi.map(
+					poi => poi.name
+				);
+				setSearchResult(names);          // ✅ 단일 setState
+
+				// setSearchResult([]);
+
+				// for (let i = 0; i < 10; i++) {
+				// 	setSearchResult( // 아래의 새로운 배열로 state를 변경합니다.
+				// 		[
+				// 			...searchResult, // 기존 배열의 모든 항목에,
+				// 			response.data.searchPoiInfo.pois.poi[i].name // 마지막에 새 항목을 추가합니다.
+				// 		]
+				// 	);
+				// }
+
+				console.log(startLocation);
+				console.log(searchResult);
+
+				return;
+			} catch (error) {
+				console.log(error);
+				return;
+			}
+		}
+
+		apiTest2();
+	}, [startLocation]);
+
+	return (
+		<ThemeProvider theme={theme}>
+			{/* 전체 화면 배경 */}
+			<GradientBackground cover={cover}>
+				{/* 상단 네비게이션 바 */}
+				<CustomAppBar isLogin={mode} />
+				<Box sx={{
+					flex: 1,
+
+					display: "flex",
+					flexDirection: "row",
+
+					width: "100%",
+					marginTop: 10,
+				}}>
+					<Box sx={{
+						flex: 2.8,
+						backgroundColor: "#F8F8F8",
+						padding: 4,
+						borderRight: "1.5px solid #AAAAAA"
+					}}>
+						<Box my={2}>
+							<Typography fontSize={24} mb={2} fontWeight="bold">
+								위치 설정
+							</Typography>
+							<Autocomplete
+								freeSolo
+								options={searchResult}
+								renderInput={(params) => <TextField {...params} fullWidth placeholder="출발지를 입력하세요." value={startLocation} onChange={(e) => setStartLocation(e.target.value)} sx={{marginBottom: 2}} />}
+							>
+							</Autocomplete>
+							{/* <TextField fullWidth placeholder="출발지를 입력하세요." value={startLocation} onChange={(e) => setStartLocation(e.target.value)} sx={{marginBottom: 2}} /> */}
+							<TextField fullWidth placeholder="도착지를 입력하세요." value={endLocation} onChange={(e) => setEndLocation(e.target.value)} sx={{
+								marginBottom: 6,
+							}} />
+							<Typography fontSize={24} mb={2} fontWeight="bold" sx={{
+							}}>
+								도착 예정 시간 설정
+							</Typography>
+							<LocalizationProvider dateAdapter={AdapterDayjs}>
+								<Stack direction="row" spacing={2} sx={{
+									justifyContent: "space-evenly",
+								}}>
+									<DesktopDatePicker label="날짜 선택" value={arrivalDate} onChange={(newValue) => setArrivalDate(newValue)} sx={{ width: 200, }} />
+									<DesktopTimePicker label="시간 선택" value={arrivalTime} onChange={(newValue) => setArrivalTime(newValue)} sx={{ width: 200, }} />
+								</Stack>
+							</LocalizationProvider>
+							<GradientButton variant="contained" onClick={handleSearch} sx={{
+								marginTop: 4,
+								borderRadius: 8,
+								paddingX: 4,
+								paddingY: 1.5,
+								width: "100%"
+							}}
+							>
+								설정 완료
+							</GradientButton>
+						</Box>
+					</Box>
+					<Stack direction="row" spacing={4} sx={{
+						flex: 7,
+						'& > *': {  // Stack의 모든 직계 자식 요소에 적용
+							flex: 1,
+							minWidth: 0  // flex item이 부모 컨테이너를 넘어가지 않도록 함
+						},
+						margin: 4,
+					}}>
+						<Card sx={{
+							borderRadius: 8,
+							border: "1.5px solid #3644C9"
+						}}>
+							<NaverMap />
+						</Card>
+						<Stack direction="column" spacing={4} sx={{
+							'& > *': {  // Stack의 모든 직계 자식 요소에 적용
+								flex: 1,
+								minWidth: 0  // flex item이 부모 컨테이너를 넘어가지 않도록 함
+							},
+							margin: 4,
+						}}>
+							<Card sx={{
+								borderRadius: 8,
+								border: "1.5px solid #AAAAAA",
+							}}>
+								<Weather />
+							</Card>
+							<Card sx={{
+								borderRadius: 8,
+								display: "flex",
+								border: "1.5px solid gray",
+							}}>
+								{/* <img src={food} style={{ width: "543px", height: "382px" }}></img> */}
+							</Card>
+						</Stack>
+					</Stack>
+				</Box>
+			</GradientBackground>
+		</ThemeProvider>
+	)
 }
 
 export default Mainpage;
